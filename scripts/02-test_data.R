8b35322da82c7c0bb957210f065417f50aab5cbf
Diana Guanzhi Liu <guanzhi.liu@mail.utoronto.ca>
2024-02-09 05:31:39 +0000

    updated tests

--- scripts/02-test_data.R
+++ scripts/02-test_data.R
@@ -3,14 +3,15 @@
 # Author: Diana
 # Date: 06 February 2024
 # Contact: guanzhi.liu@mail.utoronto.ca
-# Pre-requisites: downloaded and cleaned ACS Econ data
+# Pre-requisites: downloaded and cleaned UCSC student data
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Test data ####
-load("/cloud/project/outputs/Data/ACS_Data_UCSCEcon_Cleaned.Rda")
-cleaned_data$WAGE_INCOME|> min() >= 0
-cleaned_data$WAGE_INCOME|> max() <= 1000000
-cleaned_data$YEAR|> min() >= "2009"
-cleaned_data$AGE |> min() >= 0
+load("/cloud/project/outputs/Data/Majors_Income_Cleaned.Rda")
+cleaned_majors_data$INCWAGE|> min() >= 0
+cleaned_majors_data$INCWAGE|> max() <= 10000000
+cleaned_majors_data$YEAR|> min() >= "2009"
+cleaned_majors_data$YEAR |> max() <= "2018"
+cleaned_majors_data$Industry |> class() =="character"
